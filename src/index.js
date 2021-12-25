7fed3009a84dc0d1115671831b5cde4b7b94fdcf
ULISES <nexu_2001@hotmail.com>
2021-12-25 18:04:08 -0500

    proyecto listo para enviar a netlify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import Template from './templates/Template.js';
-console.log('hola');
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/vars.styl';
+console.log('hola como estas nexu');
+console.log('nexu')
+
+
 
 (async function App() {
   const main = null || document.getElementById('main');
